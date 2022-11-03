dcd2e1627ef49efad9a154e97335f11975b4c1b2
nithinag <nithinag10@gmail.com>
2022-11-03 15:37:57 +0530

    bot implemented

--- flask_app/main.py
+++ flask_app/main.py
@@ -3,13 +3,21 @@ from flask import Flask, request
 import hashMedia
 import database
 import json
+from twilio.twiml.messaging_response import MessagingResponse
+import requests  # request img from web
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello():
     return 'Ok'
+
+
+def respond(message):
+    response = MessagingResponse()
+    response.message(message)
+    return str(response)
 
 
 @app.route("/upload", methods=["POST"])
@@ -29,8 +37,22 @@ def postComment():
     req_string = req.decode()
     req_dict = json.loads(req_string)
     collection = database.findCollection(database="comments", collection="sub_comments")  # finding collection
-    database.insertComment(collection, req_dict['hashValue'],req_dict['comment']) # inserting comment in dbß
+    database.insertComment(collection, req_dict['hashValue'], req_dict['comment'])  # inserting comment in dbß
     return "Thanks for the comment"
+
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    sender = request.form.get('From')
+    message = request.form.get('Body')
+    media_url = request.form.get('MediaUrl0')
+    print(f'{sender} sent {message}')
+    if media_url:
+        image_string = requests.get(media_url).content
+        hex = hashMedia.getMd5Hexa(image_string)  # getting digest
+        return respond(hex)
+    else:
+        return respond(f'Please send an image!')
 
 
 if __name__ == "__main__":
